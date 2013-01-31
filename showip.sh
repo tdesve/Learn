05d0fd961dcec367aa7842be5068127700d7c16b
tdesve <tdesve@gmail.com>
2013-01-31 21:29:41 +0100

    Update showip.sh
    Explained how it works

--- showip.sh
+++ showip.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Imprimme l'adresse IP externe du routeur auquel est connecté le Raspberry Pi
+# Utilise Dyndns pour obtenir l'adresse (sous forme d'une page HTML) puis gawk pour extraire l'adresse IP
+# T. Desvé - Janvier 2013
 
-curl -s http://checkip.dyndns.org|gawk ' { gsub(/[[:alpha:]]|[<>\/ :]/,"");print  }'
+curl -s http://checkip.dyndns.org | gawk ' { gsub(/[[:alpha:]]|[<>\/ :]/,"");print  }'
